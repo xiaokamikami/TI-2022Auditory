a2538881c34967f4d8c8626817209a696ba8fa2e
xiaokamikami <38394607+xiaokamikami@users.noreply.github.com>
2022-07-29 02:20:37 +0800

    完成声波坐标测量，角度测算，云台中位

--- K210.py
+++ K210.py
@@ -1,8 +1,13 @@
 # Untitled - By: 13173 - 周三 7月 27 2022
 
-import time,math,time,sys
+import time,math,time,sys,lcd,image
+import gc,machine
+from Maix import GPIO
+from board import board_info
+from fpioa_manager import fm
 from Maix import MIC_ARRAY as mic
-from machine import Timer,PWM
+from board import board_info
+
 class Servo:
     def __init__(self, pwm, dir=50, duty_min=2.5, duty_max=12.5):
         self.value = dir
@@ -64,7 +69,7 @@ class Gimbal:
 class PID:
     _kp = _ki = _kd = _integrator = _imax = 0
     _last_error = _last_t = 0
-    _RC = 1/(2 * pi * 20)
+    _RC = 1/(2 * math.pi * 20)
     def __init__(self, p=0, i=0, d=0, imax=0):
         self._kp = float(p)
         self._ki = float(i)
@@ -105,20 +110,33 @@ class PID:
     def reset_I(self):
         self._integrator = 0
         self._last_derivative = None
-
+color_G = (0,255,0)
+def drawConfidenceText(image, rol, text, value):
+    image.draw_string(rol[0], rol[1], text , color=color_G, scale=2)
+global AngleX
+global AngleY
+global AngleR
+global Angle
+AngleX = 0
+AngleY = 0
+AngleR = 0
+Angle = 0
 def get_mic_dir():
-    AngleX=0
-    AngleY=0
-    AngleR=0
-    Angle=0
+    global AngleX
+    global AngleY
+    global AngleR
+    global Angle
+    AngleX = 0
+    AngleY = 0
+    AngleR = 0
+    Angle = 0
     AngleAddPi=0
-    mic_list=[]
+    #mic_list=[]
     imga = mic.get_map()    # 获取声音源分布图像
     imgb = imga.resize(160,160)
     imgc = imgb.to_rainbow(1)
     b = mic.get_dir(imga)   # 计算、获取声源方向
-    #显示声源图
-    lcd.display(imgc)
+
     for i in range(len(b)):
         if b[i]>=2:
             AngleX+= b[i] * math.sin(i * math.pi/6)
@@ -133,28 +151,50 @@ def get_mic_dir():
         else:
             Angle=AngleAddPi+round(math.degrees(math.atan(AngleX/AngleY)),4) #计算角度
         AngleR=round(math.sqrt(AngleY*AngleY+AngleX*AngleX),4) #计算强度
-        mic_list.append(AngleX)
-        mic_list.append(AngleY)
-        mic_list.append(AngleR)
-        mic_list.append(Angle)
-        print("强度:",AngleR)
+        #mic_list.append(AngleX)
+        #mic_list.append(AngleY)
+        #mic_list.append(AngleR)
+        #mic_list.append(Angle)
+
+
     a = mic.set_led(b,(0,0,255))# 配置 RGB LED 颜色值
-    return mic_list #返回列表，X坐标，Y坐标，强度，角度
+
+    #计算距离
+    distance = 250/(math.cos(AngleR));
+    text1 = 'D:'+str(distance)+'CM'
+    text2 = 'Roll:' + str(Angle)
+    drawConfidenceText(imgc,(0,0),text1,2)
+    drawConfidenceText(imgc,(0,24),text2,2)
+    #输出信息   测评时关闭
+    if(AngleR != 0):
+        print("x:",AngleX ,"y:",AngleY,"roll:",Angle,"强度:",AngleR,"距离",distance)
+    #显示声源图
+    lcd.display(imgc)
+
 #系统初始化
 gc.collect()    #垃圾回收器
 lcd.init()
 mic.init(i2s_d0=34, i2s_d1=8, i2s_d2=33, i2s_d3=9, i2s_ws=32, i2s_sclk=10,sk9822_dat=7, sk9822_clk=35)#可自定义配置 IO
 fm.register(21, fm.fpioa.GPIO0)
 fm.register(22, fm.fpioa.GPIO1)
-fm.register(23, fm.fpioa.GPIO2)
+
 
 #外设初始化
 img = image.Image()
-pitch_pwm = PWM(tim0, freq=50, duty=50, pin=24)#开机回中
-roll_pwm  = PWM(tim1, freq=50, duty=50, pin=25)
-gpio_key1 = GPIO(GPIO.GPIO)
-gpio_key2 = GPIO(GPIO.GPI1)
-gpio_red  = GPIO(GPIO.GPI2)
+
+pitch_pwm = machine.PWM(1, freq=50, duty=11, pin=25)#开机回中
+roll_pwm  = machine.PWM(2, freq=50, duty=20, pin=24)
+pitch_pwm.enable()
+roll_pwm.enable()
+gpio_key1 = GPIO(GPIO.GPIO0,GPIO.OUT)
+gpio_red  = GPIO(GPIO.GPIO1,GPIO.OUT)
+num = 2
+#test
+#while(num<80):
+    #num = num+1
+    #roll_pwm.duty(num)
+    #print(num)
+    #time.sleep_ms(500)
 
 '''
     servo:
@@ -167,12 +207,12 @@ gpio_red  = GPIO(GPIO.GPI2)
 '''
 
 #变量初始化
-init_pitch = 80       # init position, value: [0, 100], means minimum angle to maxmum angle of servo
+init_pitch = 50       # init position, value: [0, 100], means minimum angle to maxmum angle of servo
 init_roll = 50        # 50 means middle
 mode = 0              # 0为指向模式  1为追踪模式
 
-pitch_pid = [0.23, 0, 0.015, 0]  # P I D I_max
-roll_pid  = [0.23, 0, 0.015, 0]  # P I D I_max
+pitch_pid = [0.25, 0, 0.015, 0]  # P I D I_max
+roll_pid  = [0.25, 0, 0.015, 0]  # P I D I_max
 pitch = Servo(pitch_pwm, dir=init_pitch)
 roll = Servo(roll_pwm, dir=init_roll)
 pid_pitch = PID(p=pitch_pid[0], i=pitch_pid[1], d=pitch_pid[2], imax=pitch_pid[3])
@@ -182,52 +222,45 @@ gimbal = Gimbal(pitch, pid_pitch, roll, pid_roll)
 target_pitch = init_pitch
 target_roll = init_roll
 
-sound = [0,0,0,0]
-distance = 0
+t0 = 0
+last_run = 0
 #////////初始化完成
 while True:
+       global imgc
 # get target error
        # interval limit to > 20ms
        if time.ticks_ms() - t0 < 20:
            continue
        t0 = time.ticks_ms()
        # run
-
-
+       sound = []
        if(gpio_key1.value()==0):
            mode = 1
        else:
            mode = 0
-       sound = get_mic_dir()
-       err_roll = sound[0]
-       err_pitch = sound[1]
-
-
-
+       get_mic_dir()        #更新麦克风数据
+       err_roll = 0-AngleX
+       err_pitch = AngleY
        if( mode == 1):      #跟踪模式
            gpio_red.value(1)
-           #计算距离
-           distance = 250/(math.cos(sound[3]));
-           #输出信息
-           text = 'D:'+str(distance)+'CM'+'Roll:' + str(sound[3])
-           if(sound[2] > 2):#阈值
-                gimbal.run(err_pitch, err_roll, pitch_reverse = pitch_reverse, roll_reverse=roll_reverse)
-                image.draw_string(rol[0], rol[1], text , color=color_G, scale=2.5)
+           if(AngleR > 10):#阈值
+               gimbal.run(err_pitch, err_roll, pitch_reverse =False, roll_reverse=True)
        else :               #非跟踪
            gpio_red.value(0)
-           if(err_roll > 2):
+           if(err_roll-3< last_run and last_run < err_roll+3 ):
            #计算距离
-           distance = 250/(math.cos(sound[3]));
-           #显示信息
-           text = 'D:'+str(distance)+'CM'+'Roll:' + str(sound[3])
-           if(sound[2] > 2):#阈值
-                gimbal.run(err_pitch, err_roll, pitch_reverse = pitch_reverse, roll_reverse=roll_reverse)
-                image.draw_string(rol[0], rol[1], text , color=color_G, scale=2.5)
-           if( mode == 0):      #非跟踪模式
-                gpio_red.value(1)
-                time.sleep(1)
-                gpio_red.value(0)
+               servo_duty = AngleR *1.0/180*10+2.5
+               distance = 250/(math.cos(sound[3]))
+               #显示信息
+               text = 'D:'+str(distance)+'CM'+'Roll:' + str(Angle)
+               if(AngleR > 6):#阈值
+                    gimbal.run(err_pitch, err_roll, pitch_reverse =False, roll_reverse=True)
+               if( mode == 0):      #非跟踪模式
+                    gpio_red.value(1)
+                    time.sleep(1)
+                    gpio_red.value(0)
 
+           last_run = err_roll
 #关机
 mic.deinit()
 pwm.deinit()
